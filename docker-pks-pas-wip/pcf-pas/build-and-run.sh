e4729b110ba7d7aa770313fbbac6f3160eaf3c89
root <root@ip-172-31-76-49.ec2.internal>
2020-04-29 20:32:39 +0000

    new commit

--- docker-pks-pas-wip/pcf-pas/build-and-run.sh
+++ docker-pks-pas-wip/pcf-pas/build-and-run.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 cd nginx-reverse-proxy 
 cf push &
-echo "done"
 cd ../../microservice-kafka/microservice-kafka-order
-echo "directory changed"
 cf push &
-echo "done"
 cd ../microservice-kafka-shipping
-echo "directory changed"
 cf push &
-echo "done"
 cd ../microservice-kafka-invoicing 
 cf push &
-echo "done"
 sleep 2m
 cf apps
