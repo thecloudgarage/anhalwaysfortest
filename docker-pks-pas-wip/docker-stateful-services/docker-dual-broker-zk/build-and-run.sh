011fcbd13dbc1d55fce84ad0b83a7dd0886b1b9c
root <root@ip-172-31-42-110.ec2.internal>
2020-04-30 12:50:27 +0000

    new commit

--- docker-pks-pas-wip/docker-stateful-services/docker-dual-broker-zk/build-and-run.sh
+++ docker-pks-pas-wip/docker-stateful-services/docker-dual-broker-zk/build-and-run.sh
@@ -8,6 +8,6 @@ cd ../docker-stateful-services/docker-dual-broker-zk/
 docker-compose build
 docker-compose up -d
 sleep 2m
-docker exec mskafka_kafka_1 kafka-topics --create --topic order --replication-factor 1 --partitions 3  --zookeeper zookeeper:2181
+docker exec mskafka_kafka1_1 kafka-topics --create --topic order --replication-factor 1 --partitions 3  --zookeeper zookeeper1:32181
 curl -H "Content-Type: application/json" -X POST -d '{  "name": "order-connector",  "config": {    "connector.class":"io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",    "tasks.max": "1",    "topics": "order",    "key.ignore":"true",    "schema.ignore": "true",    "connection.url": "http://elasticsearch:9200",    "type.name": "order-type",    "name":"elasticsearch-sink"  }}' http://localhost:8083/connectors
 echo "done"
